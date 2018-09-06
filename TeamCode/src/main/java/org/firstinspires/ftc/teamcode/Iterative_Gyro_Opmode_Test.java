820fb064cb94e874ef7fcc24950934e0e894ec0b
Willexgm <wstuff@mincher.com>
2018-09-06 13:47:53 -0700

    willaisnmyseufvueyfs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Iterative_Gyro_Opmode_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Iterative_Gyro_Opmode_Test.java
@@ -62,6 +62,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
+//reeeeeeeeeeeeeeeeeee
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 @Disabled
 public class Iterative_Gyro_Opmode_Test extends OpMode
